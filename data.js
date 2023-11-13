0340e1be6663c6a91fdbb3b4d4c4ab2df6e43e4c
nani <120760985+naniar@users.noreply.github.com>
2023-11-12 19:04:10 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'shinunoga E-wa',
+        path: '',
+        artist: 'Fuji',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'DONT',
+        path: 'musics/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'musics/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'musics/Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'musics/Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'images/cover 5.png'
+    },
+]
